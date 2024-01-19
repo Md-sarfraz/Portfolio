2c6f6156884fdc15c3554a4d81afaa0a79abe0ad
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-01-19 07:12:19 +0530

    first comit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var typed=new Typed(".typing",{
-    strings:[" I'm Web devloper","I'm Freelancer","I'm Photographer","I'm Artist"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
+var typed = new Typed(".typing", {
+    strings: [" I'm Web devloper", "I'm Freelancer", "I'm Photographer", "I'm Artist"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
 })
+
+AOS.init();
