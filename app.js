9ca6bf416ad493b83b1dfe3d869e1b85c41824dd
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-01-24 22:10:21 +0530

    updated in testimonial section

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ var typed = new Typed(".typing", {
 AOS.init();
 
 
+$(document).ready(function() {
+    $('.testimonials-wrraper').owlCarousel({
+        loop: true,
+        margin:20,
+        responsiveClass: true,
+        center: false,
+        nav: true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        navText: [
+            "<i class='bi bi-arrow-left'></i>",
+            "<i class='bi bi-arrow-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 4,
+            },
+            600: {
+                items: 4,
+            },
+            1000: {
+                items: 4,
+            }
+        }
+    })
+})
